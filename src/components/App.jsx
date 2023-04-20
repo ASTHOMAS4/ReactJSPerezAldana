256309e553547c765cd42b1a0385b5ef4c5bc8cd
ASTHOMAS4 <pedro4thomas@gmail.com>
2023-04-20 18:18:15 -0300

    fin de clase 4 e instalacion de routing hecho

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,27 @@
 import './App.css'
 import {NavBar} from './NavBar/NavBar.jsx'
 import { ItemListContainer } from './ItemListContainer/ItemListContainer.jsx'
-import { Dolar } from './Dolar/Dolar.jsx'
-
-
+import { ItemDetailContainer } from './ItemDetailContainer/ItemDetailContainer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 export const App = () => {
   return (
     
     <div>
-      
-        <NavBar />
-        {/* </> */}
-        <ItemListContainer />
-        <Dolar/>
+
+        <BrowserRouter>
+          <NavBar />
+
+          <Routes>
+            <Route path='/' element= {<ItemListContainer/>} />
+            <Route path='/category/:category' element= {<ItemListContainer/>}  />
+            <Route path='/product/:id' element= {<ItemListContainer/>}  />
+            <ItemDetailContainer />
+            <ItemListContainer />
+          </Routes>
+
+
+        </BrowserRouter>
+        
     </div>
   )
 }
